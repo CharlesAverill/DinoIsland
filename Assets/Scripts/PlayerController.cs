61d016a72b7230f892a90361a4047be621ae0901
CharlesAverill <charlesaverill20@gmail.com>
2021-04-05 23:34:58 -0500

    Strafe camera mode added, debug text added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public enum CameraMode {
+        FreeLook,
+        FromBehind
+    }
+
+    List<CameraMode> camModes = new List<CameraMode>(){
+        CameraMode.FreeLook,
+        CameraMode.FromBehind
+    };
+    int camModeIndex;
+
     public Transform mainCamera;
+    public CameraMode camMode;
     public List<Transform> groundChecks;
     public Animator anim;
+
+    [HideInInspector]
+    public CharacterController cc{
+        get; private set;
+    }
+
+    [HideInInspector]
+    public Vector3 verticalVelocity;
+    [HideInInspector]
+    public Vector3 horizontalVelocity;
+
+    [HideInInspector]
+    public bool isGrounded;
 
     public float walkSpeed;
     public float turnSmoothTime;
@@ -23,10 +48,6 @@ public class PlayerController : MonoBehaviour
 
     float turnSmoothVelocity;
     float tempStepOffset;
-    bool isGrounded;
-    Vector3 verticalVelocity;
-
-    CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
@@ -35,15 +56,13 @@ public class PlayerController : MonoBehaviour
         tempStepOffset = cc.stepOffset;
         verticalVelocity = Vector3.zero;
         Cursor.lockState = CursorLockMode.Locked;
+        camModeIndex = camModes.IndexOf(camMode);
     }
 
     void Update()
     {
 
-        if (Input.GetKey("escape"))
-        {
-            Application.Quit();
-        }
+        KeyboardInput();
 
         isGrounded = checkGround();
 
@@ -51,9 +70,9 @@ public class PlayerController : MonoBehaviour
             verticalVelocity.y = 0f;
         }
 
-        Vector3 translate = Translate();
-        cc.Move(Translate());
-        if(translate.magnitude != 0f && isGrounded){
+        horizontalVelocity = Translate();
+
+        if(horizontalVelocity.magnitude != 0f && isGrounded){
             anim.SetBool("isWalking", true);
         } else {
             anim.SetBool("isWalking", false);
@@ -62,6 +81,7 @@ public class PlayerController : MonoBehaviour
         if(!isGrounded){
             anim.SetBool("isFalling", true);
             cc.stepOffset = 0f;
+            horizontalVelocity *= 0.5f;
             Fall();
         } else {
             anim.SetBool("isFalling", false);
@@ -72,9 +92,35 @@ public class PlayerController : MonoBehaviour
         if(verticalVelocity.magnitude > maxFallSpeed){
             verticalVelocity = verticalVelocity.normalized * maxFallSpeed;
         }
-        Debug.Log(verticalVelocity.magnitude);
 
-        cc.Move(verticalVelocity * Time.deltaTime);
+        cc.Move(horizontalVelocity + (verticalVelocity * Time.deltaTime));
+
+        if(camMode == CameraMode.FromBehind){
+            Vector3 currentAngle = transform.eulerAngles;
+            Vector3 targetAngle = mainCamera.transform.eulerAngles;
+
+            Vector3 newAngle = new Vector3(
+                currentAngle.x,
+                Mathf.LerpAngle(currentAngle.y, targetAngle.y, Time.deltaTime * rotateSpeed),
+                currentAngle.z
+            );
+
+            transform.eulerAngles = newAngle;
+        }
+    }
+
+    void KeyboardInput(){
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown("c")){
+            camModeIndex += 1;
+            if(camModeIndex >= camModes.Count){
+                camModeIndex = 0;
+            }
+            camMode = camModes[camModeIndex];
+        }
     }
 
     bool checkGround(){
@@ -92,24 +138,27 @@ public class PlayerController : MonoBehaviour
     }
 
     Vector3 Translate(){
-        Vector3 horizontal = mainCamera.right *
-                             Input.GetAxisRaw("Horizontal");
-        Vector3 vertical = mainCamera.forward *
-                           Input.GetAxisRaw("Vertical");
+        float hInput, vInput;
+        Vector3 horizontal, vertical;
+
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
+
+        horizontal = mainCamera.right * hInput;
+        vertical = mainCamera.forward * vInput;
+
         Vector3 moveDirection = Vector3.zero;
 
         if(vertical.magnitude > 0.1f && horizontal.magnitude > 0.1f){
             moveDirection = Vector3.Slerp(vertical, horizontal, 0.5f);
-        }
-        else if(vertical.magnitude > 0.1f){
+        } else if(vertical.magnitude > 0.1f){
             moveDirection = vertical;
-        }
-        else if(horizontal.magnitude > 0.1f){
+        } else if(horizontal.magnitude > 0.1f){
             moveDirection = horizontal;
         }
         moveDirection.y = 0f;
 
-        if(horizontal.magnitude > 0.1f && moveDirection.magnitude < 0.1f){
+        if(camMode == CameraMode.FreeLook && horizontal.magnitude > 0.1f && moveDirection.magnitude < 0.1f){
             transform.RotateAround(transform.position, transform.up, rotateSpeed);
         }
 
@@ -120,6 +169,12 @@ public class PlayerController : MonoBehaviour
                                                 targetAngle,
                                                 ref turnSmoothVelocity,
                                                 turnSmoothTime);
+
+            if(camMode == CameraMode.FromBehind){
+                // Strafe Mode
+                targetAngle = transform.eulerAngles.y;
+            }
+
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
         }
 
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+
+    public PlayerController player;
+
+    public Text cameraMode;
+    public Text position;
+    public Text velocity;
+    public Text isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(player.camMode){
+            case PlayerController.CameraMode.FreeLook:
+                cameraMode.text = "Camera Mode: Free Look";
+                break;
+            case PlayerController.CameraMode.FromBehind:
+                cameraMode.text = "Camera Mode: Strafe Look";
+                break;
+            default:
+                cameraMode.text = "Unknown Camera Mode";
+                break;
+        }
+        position.text = "Position: " + player.transform.position.ToString();
+        velocity.text = "Velocity: " +
+                        (player.horizontalVelocity +
+                            player.verticalVelocity).magnitude.ToString("F2") +
+                        " "  +
+                        player.cc.velocity.normalized.ToString();
+        isGrounded.text = "Is Grounded: " + player.isGrounded;
+    }
+}
